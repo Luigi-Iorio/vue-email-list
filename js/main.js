6e5e25d8b5257e56484fc1b23daa28dafbcf56e0
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-05 15:39:05 +0100

    feat: lista mail tramite api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      api: "https://flynn.boolean.careers/exercises/api/random/mail",
+      mailList: [],
+    };
+  },
+  methods: {},
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(this.api).then((mail) => {
+        this.mailList.push(mail.data.response);
+      });
+    }
+    console.log(this.mailList);
+  },
+}).mount("#app");
